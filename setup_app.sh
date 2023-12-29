0f9ff3ee74ae34c29e05e503ede77d17fc832ae2
John Wilger <john@johnwilger.com>
2023-12-29 12:01:57 -0800

    Update setup_app.sh script

--- setup_app.sh
+++ setup_app.sh
@@ -7,6 +7,9 @@ if [[ $# -eq 2 ]]; then
     module=$1
     app=$2
 
+    rm -rf _build
+    rm -rf .elixir_ls
+
     mv lib/basic_phx_app lib/"${app}"
     mv lib/basic_phx_app_web lib/"${app}_web"
     mv lib/basic_phx_app.ex lib/"${app}.ex"
@@ -15,20 +18,17 @@ if [[ $# -eq 2 ]]; then
 
     if [[ "$OS" == "Darwin" ]]; then
         # MacOS uses BSD sed
-        find ./{.gitignore,.envrc-sample,README.md,assets,config,lib,mix.exs,mix.lock,priv,test,./docker-compose.yml} -type f -print0 | xargs -0 sed -i '' "s/BasicPhxApp/${module}/g"
-        find ./{.gitignore,.envrc-sample,README.md,assets,config,lib,mix.exs,mix.lock,priv,test,./docker-compose.yml} -type f -print0 | xargs -0 sed -i '' "s/basic_phx_app/${app}/g"
+        find . -type f -not -path './.git/*' -print0 | xargs -0 sed -i '' "s/BasicPhxApp/${module}/g"
+        find . -type f -not -path './.git/*' -print0 | xargs -0 sed -i '' "s/basic_phx_app/${app}/g"
     else
         # Assume Linux uses GNU sed
-        find ./{.gitignore,.envrc-sample,README.md,assets,config,lib,mix.exs,mix.lock,priv,test,./docker-compose.yml} -type f -print0 | xargs -0 sed -i "s/BasicPhxApp/${module}/g"
-        find ./{.gitignore,.envrc-sample,README.md,assets,config,lib,mix.exs,mix.lock,priv,test,./docker-compose.yml} -type f -print0 | xargs -0 sed -i "s/basic_phx_app/${app}/g"
+        find . -type f -not -path './.git/*' -print0 | xargs -0 sed -i "s/BasicPhxApp/${module}/g"
+        find . -type f -not -path './.git/*' -print0 | xargs -0 sed -i "s/basic_phx_app/${app}/g"
     fi
 
     cp .envrc-sample .envrc
 
-    rm -rf _build
-
-    mix deps.get
-    mix format
+    mix do deps.get deps.compile format
     rm setup_app.sh
 else
     echo "Usage: ./setup_app.sh {ModuleName} {app_name}"
