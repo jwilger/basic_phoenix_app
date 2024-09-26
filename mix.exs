fc589bb0e31aa13003fad6b46638e37dbbd751e6
John Wilger <john@johnwilger.com>
2024-09-26 10:42:44 -0700

    Specify elixir version as 1.17

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule BasicPhxApp.MixProject do
     [
       app: :basic_phx_app,
       version: "0.1.0",
-      elixir: "~> 1.15",
+      elixir: "~> 1.17",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
